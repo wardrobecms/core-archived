389202e3bd4b027c8498f2d88353976ae04d2bcf
Ryan Tablada <ryan.tablada@gmail.com>
2013-08-03 13:42:26 -0400

    Moved migrations

---
+++ src/migrations/2013_05_31_140553_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('posts', function($table)
+		{
+			$table->increments('id');
+			$table->string('title');
+			$table->string('slug');
+			$table->text('content');
+			$table->string('image')->default('');
+			$table->string('type')->default('post');
+			$table->timestamp('publish_date');
+			$table->tinyInteger('active')->default(0);
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('posts');
+	}
+
+}
