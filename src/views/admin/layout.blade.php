f1b92671fd4ead5c2123e3bb5cd2eae08d73b903
Ryan Tablada <ryan.tablada@gmail.com>
2013-08-06 17:48:50 -0400

    Asset fix

--- src/views/admin/layout.blade.php
+++ src/views/admin/layout.blade.php
@@ -5,7 +5,7 @@
   <meta name="env" content="{{ App::environment() }}">
   <meta name="token" content="{{ Session::token() }}">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" type="text/css" href="{{ asset(wardrobe_path('admin/style.css')) }}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('/'.wardrobe_path('admin/style.css')) }}">
 </head>
 <body>
   <div id="header-region"></div>
@@ -17,9 +17,9 @@
     </div>
   </div>
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-  <script>window.jQuery || document.write('<script src="{{ asset(wardrobe_path('admin/js/jquery.js')) }} "><\/script>')</script>
-  <script type="text/javascript" src="{{ asset(wardrobe_path('admin/js/structure.js')) }}"></script>
-  <script type="text/javascript" src="{{ asset(wardrobe_path('admin/js/app.js')) }}"></script>
+  <script>window.jQuery || document.write('<script src="{{ asset('/'.wardrobe_path('admin/js/jquery.js')) }} "><\/script>')</script>
+  <script type="text/javascript" src="{{ asset('/'.wardrobe_path('admin/js/structure.js')) }}"></script>
+  <script type="text/javascript" src="{{ asset('/'.wardrobe_path('admin/js/app.js')) }}"></script>
   @yield('footer.js')
 </body>
 </html>
