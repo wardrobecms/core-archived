58361fd5f9fab4ee71c3ebaee6b9d9a8dcfcc7c9
Ryan Tablada <ryan.tablada@gmail.com>
2013-08-06 12:45:47 -0400

    Beginning routing rework

--- src/routes.php
+++ src/routes.php
@@ -2,9 +2,10 @@
 
 $wardrobeControllers = 'Wardrobe\Core\Controllers\\';
 
-Route::group(Config::get('core::routes.route_group'), function() use ($wardrobeControllers)
+Route::group(Config::get('core::routes.blog_group_rules'), function() use ($wardrobeControllers)
 {
 	Route::get('/', array('uses' => $wardrobeControllers.'HomeController@index', 'as' => 'wardrobe.index'));
+
 	Route::get('post/{slug}', $wardrobeControllers.'PostController@getShow');
 	Route::get('post/preview/{id}', $wardrobeControllers.'PostController@getPreview');
 	Route::get('tag/{tag}', $wardrobeControllers.'PostController@getTag');
