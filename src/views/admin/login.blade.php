1e724da6398c22fa626ab7cd3697eafcf868b489
Ryan Tablada <ryan.tablada@gmail.com>
2013-08-06 16:34:52 -0400

    Some HTML formatting

--- src/views/admin/login.blade.php
+++ src/views/admin/login.blade.php
@@ -9,15 +9,22 @@
 		<h1>{{ Lang::get('core::wardrobe.login') }}</h1>
 		@if (Session::has('login_errors'))
 		<div class="alert alert-block alert-error">
-			<p>{{ Lang::get('core::wardrobe.login_incorrect')}} <a href="{{ url('wardrobe/login/remind') }}">{{ Lang::get('core::wardrobe.login_forgot') }}</a></p>
+			<p>
+				{{ Lang::get('core::wardrobe.login_incorrect')}} <a href="{{ url('wardrobe/login/remind') }}">{{ Lang::get('core::wardrobe.login_forgot') }}</a>
+			</p>
 		</div>
 		@endif
 		<form method="post" action="{{ url('/wardrobe/login') }}" class="form-horizontal">
-			<p><input type="text" id="inputEmail" name="email" placeholder="{{ Lang::get('core::wardrobe.login_email') }}" value="{{ Input::old('email') }}"></p>
-			<p><input type="password" id="inputPassword" name="password" placeholder="{{ Lang::get('core::wardrobe.login_password') }}"></p>
-			<p><label class="checkbox"><input type="checkbox" name="remember"> {{ Lang::get('core::wardrobe.login_remember') }}</label></p>
+			<p>
+				<input type="text" id="inputEmail" name="email" placeholder="{{ Lang::get('core::wardrobe.login_email') }}" value="{{ Input::old('email') }}">
+			</p>
+			<p>
+				<input type="password" id="inputPassword" name="password" placeholder="{{ Lang::get('core::wardrobe.login_password') }}">
+			</p>
+			<p>
+				<label class="checkbox"><input type="checkbox" name="remember"> {{ Lang::get('core::wardrobe.login_remember') }}</label>
+			</p>
 			<button type="submit" class="btn">{{ Lang::get('core::wardrobe.login_sign_in') }}</button>
-			</div>
 		</form>
 	</div>
 @stop
