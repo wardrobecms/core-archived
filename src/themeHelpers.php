d131691582897b7fe14b312f90bd64a71f20c219
Ryan Tablada <ryan.tablada@gmail.com>
2013-08-06 12:20:51 -0400

    Theme helpers

---
+++ src/themeHelpers.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Site Title
+ *
+ * Helper that allows you to easily get the site title
+ *
+ * @return string
+ */
+function site_title()
+{
+	return Config::get('core::wardrobe.title');
+}
+
+/**
+ * Theme Path
+ *
+ * Helper that allows you to easily get a theme path inside the views.
+ * Example: @extends(theme_path('layout'))
+ *
+ * @param string $file - The file to load
+ * @return string
+ */
+function theme_path($file = null)
+{
+	return 'themes/'.Config::get('core::wardrobe.theme').'/'.$file;
+}
