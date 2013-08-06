985e051141a07e3e01d9dbde0151f140c29b6c2d
Ryan Tablada <ryan.tablada@gmail.com>
2013-08-06 12:41:19 -0400

    Migrate Post Routes

--- src/routes.php
+++ src/routes.php
@@ -5,4 +5,8 @@ $wardrobeControllers = 'Wardrobe\Core\Controllers\\';
 Route::group(Config::get('core::routes.route_group'), function() use ($wardrobeControllers)
 {
 	Route::get('/', array('uses' => $wardrobeControllers.'HomeController@index', 'as' => 'wardrobe.index'));
+	Route::get('post/{slug}', $wardrobeControllers.'PostController@getShow');
+	Route::get('post/preview/{id}', $wardrobeControllers.'PostController@getPreview');
+	Route::get('tag/{tag}', $wardrobeControllers.'PostController@getTag');
+	Route::get('archive', $wardrobeControllers.'PostController@getIndex');
 });
