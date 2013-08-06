241f944409d825f5c52b7309304b2f0dcb5ec15b
Ryan Tablada <ryan.tablada@gmail.com>
2013-08-06 15:36:44 -0400

    Migrated Facades

---
+++ src/Wardrobe/Core/Facades/Wardrobe.php
@@ -0,0 +1,48 @@
+<?php namespace Wardrobe\Core\Facades;
+
+use Config;
+use Wardrobe\Core\Repositories\PostRepositoryInterface;
+
+class Wardrobe {
+
+	/**
+	 * The post repository implementation.
+	 *
+	 * @var Wardrobe\PostRepositoryInterface
+	 */
+	protected $postsRepo;
+
+	/**
+	 * Create a new wardrobe facade instance.
+	 *
+	 * @param \Wardrobe\Facades\Wardrobe\PostRepositoryInterface|\Wardrobe\Repositories\PostRepositoryInterface $postsRepo
+	 *
+	 * @return \Wardrobe\Facades\Wardrobe
+	 */
+	public function __construct(PostRepositoryInterface $postsRepo)
+	{
+		$this->postsRepo = $postsRepo;
+	}
+
+	/**
+	 * Fetch Posts
+	 *
+	 * @param array $params
+	 * @return Posts
+	 */
+	public function posts($params = array())
+	{
+		$per_page = isset($params['per_page']) ? $params['per_page'] : Config::get('wardrobe.per_page');
+
+		return $this->postsRepo->active($per_page);
+	}
+
+	/**
+	 * Fetch all tags
+	 */
+	public function tags()
+	{
+		return $this->postsRepo->allTags();
+	}
+
+}
---
+++ src/Wardrobe/Core/Facades/WardrobeFacade.php
@@ -0,0 +1,14 @@
+<?php namespace Wardrobe\Core\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class WardrobeFacade extends Facade {
+
+		/**
+		 * Get the registered name of the component.
+		 *
+		 * @return string
+		 */
+		protected static function getFacadeAccessor() { return 'Wardrobe'; }
+
+}
