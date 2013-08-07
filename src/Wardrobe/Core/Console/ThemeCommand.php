31e4105a50611e877a3804cdce330225e0d395ed
Ryan Tablada <ryan.tablada@gmail.com>
2013-08-06 20:29:17 -0400

    No arguments

--- src/Wardrobe/Core/Console/ThemeCommand.php
+++ src/Wardrobe/Core/Console/ThemeCommand.php
@@ -42,28 +42,4 @@ class ThemeCommand extends Command {
 		passthru("mv {$assetPath} {$themePath}");
 	}
 
-	/**
-	 * Get the console command arguments.
-	 *
-	 * @return array
-	 */
-	protected function getArguments()
-	{
-		return array(
-			array('example', InputArgument::REQUIRED, 'An example argument.'),
-		);
-	}
-
-	/**
-	 * Get the console command options.
-	 *
-	 * @return array
-	 */
-	protected function getOptions()
-	{
-		return array(
-			array('example', null, InputOption::VALUE_OPTIONAL, 'An example option.', null),
-		);
-	}
-
 }
