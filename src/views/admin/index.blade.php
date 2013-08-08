3c8a59bd91c8b2c2b29e2842de06fd8eda6b193f
Ryan Tablada <ryan.tablada@gmail.com>
2013-08-07 21:17:06 -0400

    Updating for the new url requester

--- src/views/admin/index.blade.php
+++ src/views/admin/index.blade.php
@@ -12,11 +12,13 @@
 	<script type="text/javascript">
 		$(document).ready(function() {
 			Wardrobe.start({
-        user: {{ $user }},
-        users: {{ $users }},
-        base_url: "{{ route('wardrobe.api.index') }}"
-      });
+				user: {{ $user }},
+				users: {{ $users }},
+				api_url: "{{ route('wardrobe.api.index') }}"
+				admin_url: "{{ route('wardrobe.admin.index') }}"
+				blog_url: "{{ route('wardrobe.index') }}"
+			});
 		});
-    window.Lang = {@foreach($locale as $key => $item) {{ $key }}: "{{ $item }}", @endforeach}
+		window.Lang = {@foreach($locale as $key => $item) {{ $key }}: "{{ $item }}", @endforeach}
 	</script>
 @stop
