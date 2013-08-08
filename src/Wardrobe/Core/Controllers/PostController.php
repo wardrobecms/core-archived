61905d22c9b34bb81d9500f4503d2c7d0ec93809
Ryan Tablada <ryan.tablada@gmail.com>
2013-08-07 21:24:36 -0400

    Post Preview fixes

--- src/Wardrobe/Core/Controllers/PostController.php
+++ src/Wardrobe/Core/Controllers/PostController.php
@@ -1,6 +1,6 @@
 <?php namespace Wardrobe\Core\Controllers;
 
-use View, Config, Input;
+use View, Config, Input, Auth;
 use Wardrobe\Core\Repositories\PostRepositoryInterface;
 
 class PostController extends BaseController {
